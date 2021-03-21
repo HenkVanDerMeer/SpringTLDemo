fe5883241669e86658cf04df5a3d355a946480db
HenkVanDerMeer <henk.vandermeer@xs4all.nl>
2021-03-21 15:51:37 +0100

    Added Gradle

---
+++ src/main/java/com/example/SpringTLDemo/SpringTlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringTLDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringTLDemo/SpringTlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTLDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
